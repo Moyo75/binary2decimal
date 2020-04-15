0d9b341fe5722070f14bf8d7a334c070055ce264
John Ademoye <moyojohn188@gmail.com>
2020-04-15 14:41:18 +0100

    corrected the function

--- binary.js
+++ binary.js
@@ -1,38 +1,15 @@
-//INITIAL LOGIC
-const binary2Decimal = (number) => {
-  let valueArray,
-    decimal = 0,
-    arrayLength;
+export const binaryToDecimal = (number) => {
+  let decimal = 0,
+    numLength = number.length;
   const regex = /^[0-1]{1,}$/g;
 
   if (!regex.test(number)) return false;
 
-  valueArray = [...number].map(Number);
-  arrayLength = valueArray.length - 1;
-  //   console.log(valueArray);
-
-  for (let i = 0; i < valueArray.length; i++) {
-    decimal += valueArray[i] * Math.pow(2, arrayLength - i);
-    //     console.log(`${valueArray[i]} times ${2} raised to the power of ${i}`);
+  for (let i = 0; i < numLength; i++) {
+    decimal += number[i] * Math.pow(2, numLength - i - 1);
   }
 
   return decimal;
-
 };
 
-// WITHOUT ARRAY
-
-export const binaryToDecimal = (binary) => {
-  const regex = /^[0-1]{1,}$/g;
-
-  if(!regex.test(binary)) return false;
-
-  return parseInt(binary, 2);
-}
-
-// console.log(bin_to_dec(100001));
-
-
-
-
-// console.log(binaryToDecimal);
+// console.log(binaryToDecimal("100001"));
